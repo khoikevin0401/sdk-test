d591c657b86e9f665f6e0016164b4381b3ac82eb
khoi tran <khoi.tran@madison-technology.com>
2023-07-18 09:38:17 +0700

    add func fetch

--- dist/sdk.js
+++ dist/sdk.js
@@ -5,6 +5,20 @@
 
   function getInfoForm(formData) {
     console.log(formData);
+    fetch("https://jsonplaceholder.typicode.com/todos/1")
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          throw new Error("Error");
+        }
+      })
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
   }
 
   window.getInfoForm = getInfoForm;
